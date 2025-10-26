3e7e7985b444e16d8062738f1132078c0dda6b42
Dakota Owen (Bilgecrank) <8432868+Bilgecrank@users.noreply.github.com>
2025-10-26 14:52:13 -0400

    Set up central script to run everything.

--- archmatic.sh
+++ archmatic.sh
@@ -9,19 +9,24 @@
 
 . ./utils.sh
 
-
 if test -t 0; then
 	logprint 'Please provide username and password through stdin' >&2
 	exit 1
 fi
 
-read -r username password
+read -r USERNAME PASSWORD
 
 if test -z password; then
 	logprint 'Please provide a second input for the password (ex: echo "username password")' >&2
 	exit 1
 fi
 
-bash 1-pre-install.sh |& tee "$logfile"
+export USERNAME
+export PASSWORD
 
-
+bash 1-pre-install.sh |& tee -a "$logfile"
+arch-chroot /mnt git clone https://github.com/Bilgecrank/archmatic.git |& tee -a "$logfile"
+arch-chroot /mnt "${HOME}/archmatic/2-system.sh" |& tee -a "$logfile"
+arch-chroot /mnt /usr/bin/runuser -u "$USERNAME" -- git clone https://github.com/Bilgecrank/archmatic.git |& tee -a "$logfile"
+arch-chroot /mnt /usr/bin/runuser -u "$USERNAME" -- "${HOME}/archmatic/3-user.sh" |& tee -a "$logfile"
+arch-chroot /mnt /usr/bin/runuser -u "$USERNAME" -- "${HOME}/archmatic/4-setup.sh" |& tee -a "$logfile"
