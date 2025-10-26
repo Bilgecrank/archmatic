11c6d7288b522994a2f89f571f4beb0c1845f962
Dakota Owen (Bilgecrank) <8432868+Bilgecrank@users.noreply.github.com>
2025-10-26 19:18:44 -0400

    Set git cloning for repo in root.

--- archmatic.sh
+++ archmatic.sh
@@ -10,17 +10,17 @@
 . ./utils.sh
 
 if test -z "$ARCHUSER"; then
-	logprint 'Please provide username and password through stdin' >&2
+	logprint 'Please provide username by exporting ARCHUSER.' >&2
 	exit 1
 fi
 
 if test -z "$ARCHPASS"; then
-	logprint 'Please provide a second input for the password (ex: echo "username password")' >&2
+	logprint 'Please provide password by exporting ARCHPASS.' >&2
 	exit 1
 fi
 
 bash 1-pre-install.sh |& tee -a "$logfile"
-arch-chroot /mnt git clone https://github.com/Bilgecrank/archmatic.git |& tee -a "$logfile"
+arch-chroot /mnt git clone https://github.com/Bilgecrank/archmatic.git /root/archmatic |& tee -a "$logfile"
 arch-chroot /mnt "${HOME}/archmatic/2-system.sh" |& tee -a "$logfile"
 arch-chroot /mnt /usr/bin/runuser -u "$ARCHUSER" -- git clone https://github.com/Bilgecrank/archmatic.git |& tee -a "$logfile"
 arch-chroot /mnt /usr/bin/runuser -u "$ARCHUSER" -- "${HOME}/archmatic/3-user.sh" |& tee -a "$logfile"
